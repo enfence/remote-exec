8cb2b104fb082e38c6875fe89258f1fc2accbf2a
aklyachkin <aklyachkin@gmail.com>
2016-11-07 12:45:03 +0100

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# remote-exec
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: remote-exec
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
