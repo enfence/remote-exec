0c136f235533a2970488f3bb8407a3a18c42073f
Jonas Schäfer <jonas.schaefer@cloudandheat.com>
2019-08-22 14:36:06 +0200

    Make foodcritic happy

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,12 @@
 name 'remote-exec'
 maintainer 'eNFence GmbH'
 maintainer_email 'andrey.klyachkin@enfence.com'
-license 'Apache 2.0'
+license 'Apache-2.0'
 description 'remote_execute resource for Chef'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.2.0'
 issues_url 'https://github.com/enfence/remote-exec/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/enfence/remote-exec' if respond_to?(:source_url)
+chef_version '~> 12.0'
+supports 'ubuntu', '= 16.04'
+supports 'centos', '~> 7'
